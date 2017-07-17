890af83eca6077a01fcfbd1ec6f464a9579be427
Andrew Swenson <swena56@gmail.com>
2017-07-17 05:01:31 -0700

    added navigation menu fragments

--- app/src/main/java/com/deliverytips/MainActivity.java
+++ app/src/main/java/com/deliverytips/MainActivity.java
@@ -1,10 +1,10 @@
 package com.deliverytips;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +41,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
     }
 
     @Override
@@ -80,7 +84,12 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        FragmentManager fm = getFragmentManager();
+
         if (id == R.id.nav_camera) {
+
+            //Toast toast = new Toast();
+            fm.beginTransaction().replace(R.id.content_frame, new DashboardTable()).commit();
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
@@ -88,9 +97,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+            fm.beginTransaction().replace(R.id.content_frame,new Settings()).commit();
 
         }
 
